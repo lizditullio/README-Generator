0fdd289e20ed8eea806ec632501e943595d64f39
Liz DiTullio <lizdit72296@gmail.com>
2022-05-05 18:34:00 -0400

    take user input and receive in generate markdown file

---
+++ index.js
@@ -0,0 +1,87 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the project title?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Write a brief description of your project: "
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Describe the installation: ",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Describe the product usage"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Chose the appropriate license for this project: ",
+        choices: [
+            "Apache",
+            "GNU",
+            "ISC",
+            "MIT"
+        ]
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Who are the contributors of this project?"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Are there any test instructions?"
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "What do I do if I have an issue? "
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "Please enter your GitHub username: "
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email: "
+    }
+];
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function(err) {
+         if(err){
+             console.log(err)
+            }
+        console.log("succesfully wrote new file!")
+    })
+}
+
+// TODO: Create a function to initialize app
+function init() {
+   inquirer.prompt(questions)
+   .then(function(answers){
+       console.log("we have received your answers")
+       writeToFile("./README.md", generateMarkdown(answers))
+   })
+}
+
+// Function call to initialize app
+init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,24 @@
+
+
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(answers) {
+  console.log("hello from the other sideeeeee")
+  return `
+  # ${answers.title}
+  
+`;
+}
+
+module.exports = generateMarkdown;
