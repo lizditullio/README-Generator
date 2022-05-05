b0713ce9185bd5c5b42141ff68cd920dd80aede4
Liz DiTullio <lizdit72296@gmail.com>
2022-05-05 18:37:04 -0400

    Attempted to structure readme

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -18,6 +18,40 @@ function generateMarkdown(answers) {
   return `
   # ${answers.title}
   
+  ## Description
+  ${answers.description}
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+  ${answers.installation}
+
+  ## Usage
+   ${answers.usage}
+
+  ## License
+  
+  This application is covered by the ${answers.license} license. 
+
+  ## Contributers 
+ ${answers.contributing}
+
+  ## Tests
+  ${answers.tests}
+  ## Questions
+ ${answers.questions}
+  
+ Find me on GitHub: [${answers.username}]
+
+ (https://github.com/${answers.username})
+ 
+  Email me with any questions: ${answers.email}
 `;
 }
 
